fff32fee1822ea29de3b2675f1e4c8a46bcf61dc
allskiin <alysonliberal@icloud.com>
2026-01-21 01:37:29 +0100

    Add files via upload

---
+++ ex00/srcs/main.cpp
@@ -0,0 +1,29 @@
+#include "../includes/BitcoinExchange.hpp"
+#include <string>
+#include <map>
+#include <iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+//todo error msg need to be excat look at subject and need more test
+int main(int argc, char **argv)
+{
+    if(argc < 2)
+    {
+        std::cout << "Error: could not open file." << std::endl;
+        return (1);
+    } 
+    try{
+    if (argc > 2)
+        throw static_cast<std::string>("too many args");
+    BitcoinExchange  bitcoin;
+    bitcoin.handleInputFile(argv[1]);
+    // bitcoin.printMap();
+
+    }
+    catch(std::string error)
+    {
+        std::cout << "Error : " << error << std::endl;
+    }
+}
---
+++ ex01/includes/RPN.hpp
@@ -0,0 +1,27 @@
+#ifndef RPN_H
+#define RPN_H
+
+#include <string>
+#include <stack>
+#include <iostream>
+
+class RPN
+{
+    private:
+        struct{
+            double first;
+            double secend;
+        } operand;
+        double resault;
+        std::stack<double> Stack;
+    public:
+        RPN();
+        RPN(std::string rpn);
+        RPN(const RPN &src);
+        RPN & operator=(const RPN &src);
+        ~RPN();
+        bool operatorIsValid(char Operator);
+        void checkRpn(std::string &rpn, int index);
+
+};
+#endif
